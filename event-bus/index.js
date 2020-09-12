256c87a25a2f1fc050b56f995026ee102c438dcb
shockliang <shockwater@gmail.com>
2020-09-12 13:17:14 +0800

    Implementing a basic event bus.

---
+++ event-bus/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', ((req, res) => {
+  const event = req.body;
+
+  axios.post('http://localhost:4000/events', event);  // post service
+  axios.post('http://localhost:5000/events', event);  // comment service
+  axios.post('http://localhost:6000/events', event);  // query service
+
+  res.send({status: 'OK'});
+}));
+
+app.listen(7000, () => {
+  console.log('Listen on port 7000');
+})
