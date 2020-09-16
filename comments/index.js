c7dbbaf2efc64de801ee671b3657bb85a2600c46
shockliang <shockwater@gmail.com>
2020-09-17 00:02:04 +0800

    Updating comment content.

--- comments/index.js
+++ comments/index.js
@@ -35,8 +35,29 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received event', req.body.type);
+
+  const {type, data} = req.body;
+
+  if(type === 'CommentModerated') {
+    const {postId, id, status, content} = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => comment.id === id);
+    comment.status = status;
+
+    await axios.post('http://localhost:7000/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    })
+  }
+
   res.send({});
 });
 
